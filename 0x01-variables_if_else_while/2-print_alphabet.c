97017572c71430ba0522cf335d2fa1bad3868d4a
Basmacela <basmanageh44@gmail.com>
2023-04-21 14:28:18 -0700

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints the alphabet in lowercase, followed by a new line.
+ * Return: 0
+*/
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		++ch;
+	}
+	putchar('\n');
+
+	return (0);
+}
