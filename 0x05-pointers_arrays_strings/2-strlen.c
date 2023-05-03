0b6cdc9f4c1a4d61867f9d721c5fb968db424e70
Basmacela <basmanageh44@gmail.com>
2023-05-02 17:55:55 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * function that returns the length of a string
+ *
+ * Return: nothing
+ *
+*/
+int _strlen(char *s);
+{
+	int len = 0;
+
+	while (strlen != '\0')
+	{
+		len++;
+	}
+
+	return (len);
+}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * function that prints a string by new line
+ *
+ *@s: prints a string
+ *
+ * Return: nothing
+ *
+*/
+void _puts(char *str);
+{
+	int i =  0;
+	while (s[i])
+		i++;
+	
+	while (i--)
+	{
+		_putchar (s[i]);
+	}
+	_putchar ('\n');
+}
