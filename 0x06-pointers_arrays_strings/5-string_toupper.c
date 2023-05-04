e1380df5ab507d0c5095dced225504d657e404c5
Basmacela <basmanageh44@gmail.com>
2023-05-04 15:59:09 -0700

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,11 +1,9 @@
 #include "main.h"
 
 /**
- * string_toupper - changes all lowercase letters of a string to uppercase
- *
+ * string_toupper - changes all lowercase letters of a string to uppercase.
  * @n: string
- *
- * Return: n
+ * Return: n.
  */
 
 char *string_toupper(char *n)
