ed5c1c901aec8b508fe8f49d090ece7e34d96ede
Basmacela <basmanageh44@gmail.com>
2023-05-10 11:59:52 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *_print _rev_recursion
+ * @s: input
+ * Return: (0)
+*/
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar (*s);
+	}
+}
