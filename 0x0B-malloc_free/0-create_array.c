46dcb9fa4c71b37b0aaf6b8c2e4a103260b8a6cf
Basmacela <basmanageh44@gmail.com>
2023-05-27 15:03:16 -0700

    m

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,19 +10,19 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *x;
+	char *cr;
 	unsigned int i;
 
 	if (size == 0)
 		return (NULL);
 
-	x = malloc(sizeof(c) * size);
+	cr = malloc(sizeof(c) * size);
 
-	if (x == NULL)
+	if (cr == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
-		x[i] = c;
+		cr[i] = c;
 
-	return (x);
+	return (cr);
 }
