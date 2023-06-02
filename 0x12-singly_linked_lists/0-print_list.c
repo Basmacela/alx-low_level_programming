091791fbee5b7ff9333584cdc1868af4d7b68ec6
Basmacela <basmanageh44@gmail.com>
2023-06-01 17:35:40 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,17 +9,21 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nelem;
+	const list_t *current = h;
+	size_t count = 0;
 
-	nelem = 0;
-	while (h != NULL)
+	while (current != NULL)
 	{
-		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
-		else
-			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
-		nelem++;
+	if (current->str == NULL)
+	{
+		printf("[0] (nil)\n");
 	}
-	return (nelem);
+	else
+	{
+		printf("[%u] %s\n", current->len, current->str);
+	}
+	count++;
+	current = current->next;
+	}
+	return (count);
 }
