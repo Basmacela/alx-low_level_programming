74f2f374090eef7e2c644b09b816fe638971b649
Basmacela <basmanageh44@gmail.com>
2023-06-01 17:44:35 -0700

    git

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,12 +5,11 @@
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- *
+ * @len: The length of string
+ * @next: The points to next node
  * Description: singly linked list node structure
- * for project
  */
+
 typedef struct list_s
 {
 	char *str;
